7762d879671397aaa8b5b7815ac2d1161d1c85fa
cprincec <chukwuprincec@gmail.com>
2024-10-29 13:22:27 +0100

    replicated landing page zoom bug fix in other layouts

--- app/category/[categoryName]/[subCategoryName]/[secondSubCategory]/page.tsx
+++ app/category/[categoryName]/[subCategoryName]/[secondSubCategory]/page.tsx
@@ -6,43 +6,43 @@ import dynamic from "next/dynamic";
 import { useGetSecondSubCategoryFilterOptions } from "@/hooks/queries/products/filterOptions";
 
 const SecondSubCategoryProductsPage = dynamic(
-  () => import("@/components/category/SecondSubCategoriesProductsPage"),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="flex justify-center items-center h-[200px] w-screen">
-        <Loader />
-      </div>
-    ),
-  },
+    () => import("@/components/category/SecondSubCategoriesProductsPage"),
+    {
+        ssr: false,
+        loading: () => (
+            <div className="flex justify-center items-center h-[200px] w-screen">
+                <Loader />
+            </div>
+        ),
+    }
 );
 
 export default async function Page({
-  params,
+    params,
 }: {
-  params: {
-    categoryName: string;
-    subCategoryName: string;
-    secondSubCategory: string;
-  };
+    params: {
+        categoryName: string;
+        subCategoryName: string;
+        secondSubCategory: string;
+    };
 }) {
-  const { categoryName, subCategoryName, secondSubCategory } = params;
-  const { filterOptions } = useGetSecondSubCategoryFilterOptions(
-    decodeURIComponent(categoryName),
-    decodeURIComponent(subCategoryName),
-    decodeURIComponent(secondSubCategory),
-  );
+    const { categoryName, subCategoryName, secondSubCategory } = params;
+    const { filterOptions } = useGetSecondSubCategoryFilterOptions(
+        decodeURIComponent(categoryName),
+        decodeURIComponent(subCategoryName),
+        decodeURIComponent(secondSubCategory)
+    );
 
-  return (
-    <InnerPageLayout>
-      <div className="lg:my-10 my-24 space-y-5 min-h-[calc(100vh-8rem)]">
-        <SecondSubCategoryProductsPage
-          Category={decodeURIComponent(categoryName)}
-          SubCategory={decodeURIComponent(subCategoryName)}
-          SecondSubCategory={decodeURIComponent(secondSubCategory)}
-          filterOptions={filterOptions}
-        />
-      </div>
-    </InnerPageLayout>
-  );
+    return (
+        <InnerPageLayout>
+            <div className="lg:my-10 my-24 space-y-5 min-h-[calc(100vh-8rem)]">
+                <SecondSubCategoryProductsPage
+                    Category={decodeURIComponent(categoryName)}
+                    SubCategory={decodeURIComponent(subCategoryName)}
+                    SecondSubCategory={decodeURIComponent(secondSubCategory)}
+                    filterOptions={filterOptions}
+                />
+            </div>
+        </InnerPageLayout>
+    );
 }
--- app/category/[categoryName]/[subCategoryName]/page.tsx
+++ app/category/[categoryName]/[subCategoryName]/page.tsx
@@ -6,49 +6,45 @@ import dynamic from "next/dynamic";
 import { useGetSubCategoryFilterOptions } from "@/hooks/queries/products/filterOptions";
 import useGetAvailableSecondSubCategories from "@/hooks/queries/category/getAvailableSecondSubCategories";
 
-const SubCategoryProductsPage = dynamic(
-  () => import("@/components/category/SubCategoriesProductsPage"),
-  {
+const SubCategoryProductsPage = dynamic(() => import("@/components/category/SubCategoriesProductsPage"), {
     ssr: false,
     loading: () => (
-      <div className="flex justify-center items-center h-[200px] w-screen">
-        <Loader />
-      </div>
+        <div className="flex justify-center items-center h-[200px] w-screen">
+            <Loader />
+        </div>
     ),
-  },
-);
+});
 
 export default async function Page({
-  params,
+    params,
 }: {
-  params: { categoryName: string; subCategoryName: string };
+    params: { categoryName: string; subCategoryName: string };
 }) {
-  const { categoryName, subCategoryName } = params;
-  const { filterOptions } = useGetSubCategoryFilterOptions(
-    decodeURIComponent(categoryName),
-    decodeURIComponent(subCategoryName),
-  );
-
-  const { allCategories, fetchingAllCategories } =
-    useGetAvailableSecondSubCategories(
-      decodeURIComponent(categoryName),
-      decodeURIComponent(subCategoryName),
+    const { categoryName, subCategoryName } = params;
+    const { filterOptions } = useGetSubCategoryFilterOptions(
+        decodeURIComponent(categoryName),
+        decodeURIComponent(subCategoryName)
     );
 
-  if (filterOptions && !fetchingAllCategories && allCategories) {
-    filterOptions.secondSubCategory = allCategories;
-  }
+    const { allCategories, fetchingAllCategories } = useGetAvailableSecondSubCategories(
+        decodeURIComponent(categoryName),
+        decodeURIComponent(subCategoryName)
+    );
 
-  return (
-    <InnerPageLayout>
-      <div className="lg:my-10 my-24 space-y-5 min-h-[calc(100vh-8rem)]">
-        <SubCategoryProductsPage
-          Category={decodeURIComponent(categoryName)}
-          SubCategory={decodeURIComponent(subCategoryName)}
-          SecondSubCategory=""
-          filterOptions={filterOptions}
-        />
-      </div>
-    </InnerPageLayout>
-  );
+    if (filterOptions && !fetchingAllCategories && allCategories) {
+        filterOptions.secondSubCategory = allCategories;
+    }
+
+    return (
+        <InnerPageLayout>
+            <div className="lg:my-10 my-24 space-y-5 min-h-[calc(100vh-8rem)]">
+                <SubCategoryProductsPage
+                    Category={decodeURIComponent(categoryName)}
+                    SubCategory={decodeURIComponent(subCategoryName)}
+                    SecondSubCategory=""
+                    filterOptions={filterOptions}
+                />
+            </div>
+        </InnerPageLayout>
+    );
 }
